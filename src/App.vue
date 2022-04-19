b3b0118fe3bb0ff2bfd4cea480b785d989efb233
erwin <winzon353535@gmail.com>
2022-04-19 10:38:19 -0700

    adds NoteContainer.vue with content

--- src/App.vue
+++ src/App.vue
@@ -11,31 +11,6 @@ export default {
 <template>
   <div id="app">
     <NoteToolbar />
-    <div class="note-container">
-      <div class="note-selectors">
-        <div class="note-selector active">
-          <p class="note-selector-title">First note...</p>
-          <p class="note-selector-timestamp">Timestamp here...</p>
-        </div>
-        <div class="note-selector">
-          <p class="note-selector-title">Second note...</p>
-          <p class="note-selector-timestamp">Timestamp here...</p>
-        </div>
-        <div class="note-selector">
-          <p class="note-selector-title">Third note...</p>
-          <p class="note-selector-timestamp">Timestamp here...</p>
-        </div>
-      </div>
-      <div class="note-editor">
-        <p class="note-editor-info">Timestamp here...</p>
-        <textarea class="note-editor-input">
-	          First note...
-
-	          Note text here...
-	        </textarea
-        >
-      </div>
-    </div>
   </div>
 </template>
 
