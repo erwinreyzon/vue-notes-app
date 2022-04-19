98719c51631d49a52fdef8726c09f1d991b73e41
erwin <winzon353535@gmail.com>
2022-04-19 11:54:04 -0700

    adds v-for loop for notes in NoteSelectors.vue and props to NoteSelector.vue

--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -2,6 +2,15 @@
 import NoteSelector from "./NoteSelector.vue";
 
 export default {
+  data: function () {
+    return {
+      notes: [
+        { id: 1, body: "First Note", timestamp: Date.now() },
+        { id: 2, body: "Second Note", timestamp: Date.now() },
+        { id: 3, body: "Third Note", timestamp: Date.now() },
+      ],
+    };
+  },
   components: {
     NoteSelector,
   },
@@ -10,8 +19,6 @@ export default {
 
 <template>
   <div class="note-selectors">
-    <NoteSelector />
-    <NoteSelector />
-    <NoteSelector />
+    <NoteSelector v-for="note in notes" v-bind:note="note" v-bind:key="note.id" />
   </div>
 </template>
